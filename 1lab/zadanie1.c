6bde812639d74c81ebf544c9fe2c1e8cd4ba3bca
Wlitkopa <przemek@student.agh.edu.pl>
2022-03-16 14:07:53 +0100

    16.03.2022 trying to lear C structure after deleating my previous work

--- 1lab/zadanie1.c
+++ 1lab/zadanie1.c
@@ -1,6 +1,46 @@
-#include <stdio.h>
 
-int main() {
-    printf("Hello, World !!\n");
+#include <stdio.h>
+#include <time.h>
+#include <complex.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+int main(){
+
+    int n;
+    int tempn;
+    int i = 1;
+    int length = 0;
+    int power;
+    int intsum = 0;
+    int lastdigit;
+    int templength;
+    printf("Podaj liczbę: ");
+    scanf("%d", &n);
+    length = (int)floorf(log10f((float)n)) + 1;
+    printf("Liczba cyfr w n: %d \n", length);
+    tempn = n;
+    templength = length;
+    while(templength > 0){
+        lastdigit = tempn%10;
+        printf("Last digit: %d \n", lastdigit);
+        intsum = intsum + powf(lastdigit, length);
+        printf("intsum: %d \n", intsum);
+        tempn = tempn - lastdigit;
+        tempn = tempn/10;
+        printf("tempn: %d \n", tempn);
+        templength--;
+        i++;
+    }
+    if(intsum == n){
+        printf("%d jest liczbą Armstronga \n", n);
+    }
+    else{
+        printf("%d nie jest liczbą Armstronga \n", n);
+
+    }
+
+
     return 0;
 }
