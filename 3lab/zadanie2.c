1cdcde1b067a5cfea930a6dda4da1ae9200bc798
Wlitkopa <przemek@student.agh.edu.pl>
2022-04-14 21:36:13 +0200

    lab3 tasks

--- 3lab/zadanie2.c
+++ 3lab/zadanie2.c
@@ -10,47 +10,151 @@ char* getHexAscii(const char ch[]){
     return val;
 }
 
-char** getData(void){
+char** getData(int *len, int *largest){
+
 
     char **first = (char**) malloc(3*sizeof (char*));
-    char *buffer;
-    int rtrn;
+    char *buffer = NULL;
+    *len = 0;
+    printf("len1: %d\n", *len);
+    *largest = 0;
+
     size_t bufsize = 32;
     size_t chars;
     buffer = (char*)malloc(bufsize*sizeof(char));
 
-    for (int i = 0; i < 3; ++i) {
+    while (1) {
         chars = getline(&buffer, &bufsize, stdin);
-        first[i] = (char*) malloc(chars*sizeof (char));
+        if ((int)chars > *largest){
+            *largest = (int)chars;
+        }
+        printf("len: %d\n", *len);
+
+        if (*len > 2){
+            first = (char**) realloc(first, (*len + 1)*sizeof (char*));
+        }
+        if (buffer[chars - 1] != '\n')
+            chars += 1;
+        buffer[chars - 1] = 0;
+
         printf("Chars: %zu \nBuffer: %s\n", chars, buffer);
-        if(chars == -1){
-            first[i] = NULL;
+
+        if ((chars == -1) || (strlen(buffer) == 0)){
+            first[*len] = NULL;
+            printf("NULL has beed added\n\n");
+            if (*len >= 3){
+                break;
+            }
         }
         else{
-            first[i] = buffer;
-            printf("buffer: %s \n", buffer);
+            first[*len] = (char*) malloc(chars*sizeof (char));
+
+            memcpy(first[*len], buffer, chars * sizeof(char));
         }
-        printf("You've typed %s \n", first[i]);
+        printf("You've typed %s\n\n", first[*len]);
+        *(len) += 1;
 
     }
-    for (int i = 0; i < 3; ++i) {
-        printf("You've typed %s \n", first[i]);
+    for (int i = 0; i < *len; ++i) {
+        if (first[i] != NULL){
+            printf("You've typed: %s\n", first[i]);
+            printf("strlen: %d\n\n", (int)strlen(first[i]));
+        }
     }
-    for (int i = 0; i < 3; ++i) {
-        free(first[i]);
-    }
-    free(first);
+    printf("exit\n");
 
-//    return first;
+    return first;
 
 }
+
+
+char* compress(char** first, const int* len, const int* largest){
+
+    printf("start1\n");
+    printf("largest: %d\n", *largest);
+    printf("len: %d\n", *len);
+    char *comp = (char*) malloc((*len)*(*largest) * sizeof (char));
+    int length;
+    char ch;
+    int p = 0;
+    printf("start2\n\n");
+
+    for (int i = 0; i < *len; ++i) {
+
+        if (first[i] != NULL){
+        length = (int)strlen(first[i]);
+        int cnt = 1;
+
+
+            for (int j = 0; j < length; ++j) {
+//                printf("First[%d]: \n %s \n\n", i, first[i]);
+//                printf("Careful: %c\n", first[i][j]);
+                ch = first[i][j];
+                if (first[i][j] == first[i][j+1]){
+                    cnt += 1;
+                }
+
+                else{
+                    if (ch == '(' || ch == ')' || ch == '%'){
+                        printf("else if brackets or percentage\n");
+                        comp[p++] = '%';
+                        sprintf(&comp[p++], "%d", *(getHexAscii(&ch)));
+                        sprintf(&comp[p++], "%d", *(getHexAscii(&ch) + 1));
+
+                        printf("Ascii: %d for char: %c\n", *(getHexAscii(&ch)), ch);
+//                        comp[p++] = *getHexAscii(&ch);
+                        comp[p++] = '(';
+                        sprintf(&comp[p++], "%d", cnt);
+//                        comp[p++] = (char)cnt;
+                        comp[p++] = ')';
+                    }
+                    else{
+                        if (cnt < 5){
+                            for (int k = 0; k < cnt; ++k) {
+                                comp[p++] = ch;
+                            }
+                        }
+                        else{
+                            sprintf(&comp[p++], "%c", ch);
+//                            comp[p++] = ch;
+                            comp[p++] = '(';
+                            sprintf(&comp[p++], "%d", cnt);
+//                            comp[p++] = (char)cnt;
+                            comp[p++] = ')';
+                        }
+                    }
+                    cnt = 1;
+                }
+
+            }
+            comp[p++] = '\n';
+
+        }
+
+    }
+
+    return comp;
+
+}
+
 
 
 int main(){
 
     char choice[] = "%";
-    int linecnt = 3;
     printf("Value of %s in ASCII is %d\n", choice, *getHexAscii(choice));
-    getData();
+    int *len = (int*) malloc(sizeof (int));
+    int *largest = (int*) malloc(sizeof (int));
+    char **tekst;
+    char *compressed;
+    tekst = getData(len, largest);
+    compressed = compress(tekst, len, largest);
+    printf("Finaly:\n%s", compressed);
 
+    //    for (int i = 0; i < 3; ++i) {
+    //        free(first[i]);
+    //    }
+    //    free(first);
+
+// 1. Naruszenie ochrony pamięci gdy wrzucam np dwa NULLe 2. Nie podaje ilości wystąpień znaków 3.
 }
